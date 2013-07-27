477831b29ec51da3e8e32e9b5b6f833b183eb14c
prungta <prungta@gmail.com>
2013-07-26 19:46:40 -0700

    added default.html and upload.php scripts

---
+++ upload.php
@@ -0,0 +1,24 @@
+<?php 
+ $target = "uploaddir/"; 
+ $target = $target . basename( $_FILES['uploaded']['name']) ; 
+ $ok=1; 
+ 
+ //make sure ok is 1
+ if ($ok==0) 
+ { 
+ Echo "Sorry your file was not uploaded"; 
+ } 
+ 
+ //Uploading happens here
+ else 
+ { 
+ if(move_uploaded_file($_FILES['uploaded']['tmp_name'], $target)) 
+ { 
+ echo "The file ". basename( $_FILES['uploadedfile']['name']). " has been uploaded"; 
+ } 
+ else 
+ { 
+ echo "Sorry, there was a problem uploading your file. You fail."; 
+ } 
+ } 
+ ?> 
